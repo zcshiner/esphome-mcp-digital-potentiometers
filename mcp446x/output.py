42ce9fc894cbc9004b8192fe6c81423e8082cc8e
Zach Shiner <1305096+zcshiner@users.noreply.github.com>
2025-12-30 16:44:09 -0500

    add initial automations to mcp446x - compiles with quad channel test yaml

--- mcp446x/output.py
+++ mcp446x/output.py
@@ -1,8 +1,10 @@
 import esphome.codegen as cg
 from esphome.components import output
 import esphome.config_validation as cv
-from esphome.const import CONF_CHANNEL, CONF_ID
+from esphome.const import CONF_CHANNEL, CONF_ID, CONF_LEVEL
 from .. import mcp4xxx_digipot_base
+from esphome import automation
+from . import DIGIPOT_TAPS
 
 DEPENDENCIES = ["mcp4xxx_digipot_i2c"]
 CONF_MCP4XXX_ID = "mcp446x_id"
@@ -10,6 +12,9 @@ CONF_MCP4XXX_ID = "mcp446x_id"
 mcp4xxx_wiper = mcp4xxx_digipot_base.mcp4xxx_digipot_base_ns.class_(
     "MCP4XXXWiper", output.FloatOutput, cg.Parented.template(mcp4xxx_digipot_base.mcp4xxx_digipot_base_component)
 )
+IncreaseAction = mcp4xxx_digipot_base.mcp4xxx_digipot_base_ns.class_("IncreaseAction", automation.Action)
+DecreaseAction = mcp4xxx_digipot_base.mcp4xxx_digipot_base_ns.class_("DecreaseAction", automation.Action)
+SetWiperValueAction = mcp4xxx_digipot_base.mcp4xxx_digipot_base_ns.class_("SetWiperValueAction", automation.Action)
 
 CHANNEL_OPTIONS = {
     '0': mcp4xxx_digipot_base.MCP4XXXWiperID.WIPER_0,
@@ -29,6 +34,34 @@ CONFIG_SCHEMA = (
     )
     .extend(mcp4xxx_digipot_base.OUTPUT_OPTIONAL_CONFIG_SCHEMA)
 )
+
+INCREASE_DECREASE_ACTION_SCHEMA = automation.maybe_simple_id(
+        {
+            cv.Required(CONF_ID): cv.use_id(mcp4xxx_wiper),
+        }
+    )
+
+SET_LEVEL_ACTION_SCHEMA = cv.Schema(
+        {
+            cv.Required(CONF_ID): cv.use_id(mcp4xxx_wiper),
+            cv.Required(CONF_LEVEL): cv.templatable(cv.int_range(min=0, max=DIGIPOT_TAPS)),
+        }
+    )
+
+@automation.register_action("output.increment_wiper", IncreaseAction, INCREASE_DECREASE_ACTION_SCHEMA)
+async def output_increase_wiper(config, condition_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(condition_id, template_arg, paren)
+
+@automation.register_action("output.decrement_wiper", DecreaseAction, INCREASE_DECREASE_ACTION_SCHEMA)
+async def output_decrease_wiper(config, condition_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(condition_id, template_arg, paren)
+
+@automation.register_action("output.set_wiper_level", SetWiperValueAction, SET_LEVEL_ACTION_SCHEMA)
+async def output_set_wiper_level(config, condition_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    return cg.new_Pvariable(condition_id, template_arg, paren)
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_MCP4XXX_ID])
