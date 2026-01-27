972e9f2b2c6266b1db705f64b0ac974ff492a4fc
Zach Shiner <1305096+zcshiner@users.noreply.github.com>
2026-01-26 21:32:57 -0500

    Verbose log what NV register was before writing new value

--- components/mcp4xxx_digipot_base/mcp4xxx_digipot_base.cpp
+++ components/mcp4xxx_digipot_base/mcp4xxx_digipot_base.cpp
@@ -71,6 +71,8 @@ bool mcp4xxx_digipot_base_component::set_wiper_value_(MCP4XXXWiperID wiper, uint
       default:
         break;
     }
+    ESP_LOGV(TAG, "Previous non-volatile wiper %d was tap %d of %d",
+            wiper, this->read_wiper_value_(wiper_address), MCP4XXX_MAX_VALUE);
     ESP_LOGV(TAG, "Setting non-volatile wiper %d to tap %d of %d", wiper, value, MCP4XXX_MAX_VALUE);
   } else {
     ESP_LOGV(TAG, "Setting volatile wiper %d to tap %d of %d", wiper, value, MCP4XXX_MAX_VALUE);
